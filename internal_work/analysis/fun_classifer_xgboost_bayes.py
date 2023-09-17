d0fe0ca682eb9f8101772dfce4187a49a0197da6
mhanauer <matthewhanauer99@gmail.com>
2023-09-17 07:46:54 -0400

    Update fun_classifer_xgboost_bayes.py

--- internal_work/analysis/fun_classifer_xgboost_bayes.py
+++ internal_work/analysis/fun_classifer_xgboost_bayes.py
@@ -24,6 +24,21 @@ def fun_classifer_xgboost_bayes(
         n_repeats (int): The number of repeats for the repeated cross validation.
         id_var (str, optional): Id var you need to remove.  Likely mrn. Defaults to "None".
 
+learning_rate: (0.01, 0.3)
+    Explanation: This parameter controls how quickly or slowly a model learns during training. A smaller value like 0.01 makes the model        learn slowly, which could achieve better performance but may also lead to overfitting if not properly regulated. A larger value like        0.3 makes the model learn quickly, potentially making the learning process less accurate.
+max_depth: (2, 5)
+    Explanation: This parameter specifies the maximum depth of the tree in the ensemble model. A small depth can result in a simpler model      (may underfit), while a larger depth can capture more complexity but may overfit. The range here is from 2 to 5, allowing trees to be       moderately complex.
+subsample: (0.3, 0.7)
+    Explanation: This parameter defines the proportion of the training data to randomly sample in each boosting round. Subsampling helps in     making the model more robust by preventing overfitting. Values range between 0.3 and 0.7, meaning each boosting round uses 30% to 70%       of the data.
+colsample_bytree: (0.1, 0.7)
+    Explanation: This parameter indicates the fraction of features to choose for each boosting round. It's a way to perform feature         
+    sampling, providing an extra layer of randomness and thus making the model more robust. The range between 0.1 and 0.7 implies using 10%     to 70% of features for each round.
+reg_lambda: (1, 3.0)
+    Explanation: This is the L2 regularization term on weights, used to avoid overfitting. Increasing this value will make the model more       conservative by constraining the weight values, making the model simpler but potentially underfitting. The range is between 1 and 3,        suggesting a moderate level of regularization.
+
+"hist" (Histogram-based)
+    Explanation: The histogram-based approach buckets continuous feature values into discrete bins and builds histograms during training.       It speeds up the training process considerably and is useful for datasets with a large number of rows but can result in slightly lower      accuracy due to the binning process.
+
     Returns:
          Model: Returns the best model based on highest auc.
     """
